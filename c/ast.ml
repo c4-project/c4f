c5b90a63e82d69c4422f73618dd647a1fed490ae
Matt Windsor <m.windsor@imperial.ac.uk>
2018-12-18 12:11:51 +0000

    C: Remove existing surplus Herd stuff.

--- c/ast.ml
+++ c/ast.ml
@@ -14,18 +14,6 @@
 (* "http://www.cecill.info". We also give a copy in LICENSE.txt.            *)
 (****************************************************************************)
 
-type param = { param_ty : C_type.t; param_name : string }
-
-type 'body test =
-  { proc : int
-  ; params : param list
-  ; body : 'body
-  }
-
-type 'body t =
-  | Global of string
-  | Test of 'body test
-
 (* New AST, under construction *)
 
 module Identifier = struct
