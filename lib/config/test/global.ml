f2cd3f75c7500e159b0d2f2e686cfa11ed655bf1
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-05 13:54:11 +0100

    Config: add tests for global
    These don't do much, but at least they witness the presence of
    accessors?  Idk.

---
+++ lib/config/test/global.ml
@@ -0,0 +1,82 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+open Base
+open Stdio
+
+open Act_config
+module Ac = Act_common
+module Acc = Act_compiler
+
+let%test_module "accessors" = (module struct
+  let cpp : Cpp.t = Cpp.default ()
+
+  let fuzz : Fuzz.t = Fuzz.make ()
+
+  let defaults : Ast.Default.t list =
+    Ast.Default.[ Try (Compiler, Ac.Id.of_string "localhost.gcc.x86.normal")
+                ; Try (Arch, Ac.Id.of_string "x86.att") ]
+
+  let localhost_gcc_spec : Acc.Spec.t =
+    Acc.Spec.make ~cmd:"gcc" ~style:(Ac.Id.of_string "gcc")
+      ~emits:(Ac.Id.of_string "x86.att")
+      ~enabled:true
+      ()
+
+  let localhost_compilers : Acc.Spec.Set.t =
+    Or_error.ok_exn
+      (Acc.Spec.Set.of_list
+         [ Acc.Spec.With_id.make
+             ~id:(Ac.Id.of_string "gcc.x86.normal")
+             ~spec:localhost_gcc_spec ])
+
+  let localhost_herd_spec : Act_sim.Spec.t =
+    Act_sim.Spec.make ~cmd:"herd7" ~style:(Ac.Id.of_string "herd")
+      ()
+
+  let localhost_sims : Act_sim.Spec.Set.t =
+    Or_error.ok_exn
+      (Act_sim.Spec.Set.of_list
+         [ Act_sim.Spec.With_id.make
+             ~id:(Ac.Id.of_string "herd")
+             ~spec:localhost_herd_spec ])
+
+  let localhost_spec : Acc.Machine_spec.t =
+    Acc.Machine_spec.make ~enabled:true ~via:Acc.Via.local
+      ~compilers:localhost_compilers ~sims:localhost_sims ()
+
+  let machines : Acc.Machine_spec.Set.t =
+    Or_error.ok_exn
+      (Acc.Machine_spec.Set.of_list
+         [ Acc.Machine_spec.With_id.make
+             ~id:(Ac.Id.of_string "localhost")
+             ~spec:localhost_spec ])
+        
+  let global : Global.t =
+    Global.make ~cpp ~fuzz ~defaults ~machines ()
+
+  let%expect_test "cpp" =
+    print_s [%sexp (Global.cpp global : Cpp.t option)];
+    [%expect {| (((enabled true) (cmd ()) (argv ()))) |}]
+
+  let%expect_test "defaults" =
+    print_s [%sexp (Global.defaults global : Ast.Default.t list)];
+    [%expect {| ((Try Compiler (localhost gcc x86 normal)) (Try Arch (x86 att))) |}]
+
+  let%expect_test "fuzz" =
+    print_s [%sexp (Global.fuzz global : Fuzz.t option)];
+    [%expect {| (((weights ()))) |}]
+
+  let%expect_test "machines" =
+    Fmt.pr "@[%a@]@." Act_compiler.Machine_spec.Set.pp (Global.machines global);
+    [%expect {| localhost: local |}]
+
+end)
