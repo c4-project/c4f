d8330fe5e30f36932dc47d8334c66a78636b1542
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-05 13:54:11 +0100

    Config: add tests for global
    These don't do much, but at least they witness the presence of
    accessors?  Idk.

---
+++ lib/config/test/global.mli
@@ -0,0 +1,14 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+(** Tests over the 'global' configuration struct.
+
+    This interface intentionally left blank. *)
