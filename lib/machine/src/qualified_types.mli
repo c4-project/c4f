d5d8ed80f3b97282d6822dea3eb7bc25856227b6
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-06 14:13:19 +0100

    Machine: add qualified lookup tests
    These don't give *quite* the right output yet.

---
+++ lib/machine/src/qualified_types.mli
@@ -0,0 +1,46 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+(** Module types for machine-qualified specifications and their lookup.
+
+    Generally, user code won't need to use this module directly; the salient
+    parts are imported/used in {{!Qualified} Qualified}. *)
+
+open Base
+
+(** {2 Types for qualified specs} *)
+
+module type S = sig
+  (** Opaque type of the qualified specification itself. *)
+  type t [@@deriving equal]
+
+  (** {3 Accessors} *)
+
+  val m_spec : t -> Spec.With_id.t
+  (** [m_spec spec] accesses the bundled machine specification inside
+      [spec]. *)
+end
+
+(** {2 Types for lookups} *)
+
+(** Signature of modules that implement qualified lookup. *)
+module type S_lookup = sig
+  (** The qualified final specification type. *)
+  type t
+
+  val lookup : Spec.Set.t -> fqid:Act_common.Id.t -> t Or_error.t
+  (** [lookup machines ~fqid] looks up a specification using its fully
+      qualified ID [fqid] in [machines]. *)
+
+  val all : Spec.Set.t -> t list
+  (** [all machines] returns a list of qualified forms of all specifications
+      reachable from the machines in [machines]. *)
+end
---
+++ lib/machine/test/qualified.ml
@@ -0,0 +1,63 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+open Base
+
+let print_result (type a) (pp_inner : a Fmt.t) : a Or_error.t -> unit =
+  Fmt.(pr "@[%a@]@." (result ~ok:pp_inner ~error:Error.pp))
+
+let%test_module "compiler lookup" =
+  ( module struct
+    let machines = Lazy.force Data.Spec_sets.single_local_machine
+
+    let test (fqid : Act_common.Id.t) : unit =
+      let result =
+        Act_machine.Qualified.Lookup_compilers.lookup machines ~fqid
+      in
+      print_result
+        (Fmt.using Act_machine.Qualified.Compiler.c_spec
+           Act_compiler.Spec.With_id.pp)
+        result
+
+    let%expect_test "positive example" =
+      test (Act_common.Id.of_string "localhost.gcc.x86.normal") ;
+      [%expect {|
+        Style: gcc
+        Emits: x86.att
+        Command: gcc |}]
+
+    let%expect_test "negative example" =
+      test (Act_common.Id.of_string "localhost.clang.x86.O3") ;
+      [%expect {|
+        ("unknown ID" (of_type compiler) (id (clang x86 O3))
+         (suggestions ((gcc x86 normal)))) |}]
+  end )
+
+let%test_module "sim lookup" =
+  ( module struct
+    let machines = Lazy.force Data.Spec_sets.single_local_machine
+
+    let test (fqid : Act_common.Id.t) : unit =
+      let result =
+        Act_machine.Qualified.Lookup_sims.lookup machines ~fqid
+      in
+      print_result
+        (Fmt.using Act_machine.Qualified.Sim.s_spec Act_sim.Spec.With_id.pp)
+        result
+
+    let%expect_test "positive example" =
+      test (Act_common.Id.of_string "localhost.herd") ;
+      [%expect {| TODO |}]
+
+    let%expect_test "negative example" =
+      test (Act_common.Id.of_string "localhost.litmus") ;
+      [%expect {| ("unknown ID" (of_type sim) (id (litmus)) (suggestions ((herd)))) |}]
+  end )
