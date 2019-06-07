d68c323db25c4e5a6c8847b4cd0ab2fec4d7a8cd
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-07 16:20:53 +0100

    Machine/Qualified: add defaults lookup
    Machine-qualified lookups now take an optional list of machine IDs to
    try if the original ID doesn't resolve to anything on its own.
    
    This hasn't yet been connected to the 'default' stanzas in act's config,
    but that's the next job.

--- lib/machine/test/qualified.ml
+++ lib/machine/test/qualified.ml
@@ -18,9 +18,11 @@ let%test_module "compiler lookup" =
   ( module struct
     let machines = Lazy.force Data.Spec_sets.single_local_machine
 
-    let test (fqid : Act_common.Id.t) : unit =
+    let test ?(defaults : Act_common.Id.t list option)
+        (fqid : Act_common.Id.t) : unit =
       let result =
         Act_machine.Qualified.Lookup_compilers.lookup machines ~fqid
+          ?defaults
       in
       print_result
         (Fmt.using Act_machine.Qualified.Compiler.c_spec
@@ -29,6 +31,18 @@ let%test_module "compiler lookup" =
 
     let%expect_test "positive example" =
       test (Act_common.Id.of_string "localhost.gcc.x86.normal") ;
+      [%expect
+        {|
+        Style: gcc
+        Emits: x86.att
+        Command: gcc |}]
+
+    let%expect_test "positive example: defaults resolution" =
+      test
+        (Act_common.Id.of_string "gcc.x86.normal")
+        ~defaults:
+          Act_common.Id.
+            [of_string "foo"; of_string "bar"; of_string "localhost"] ;
       [%expect
         {|
         Style: gcc
@@ -47,6 +61,24 @@ let%test_module "compiler lookup" =
       [%expect
         {|
         ("unknown ID" (of_type machine) (id (kappa gcc x86 normal))
+         (suggestions ((localhost)))) |}]
+
+    let%expect_test "negative example: missing machine" =
+      test (Act_common.Id.of_string "gcc.x86.normal") ;
+      [%expect
+        {|
+        ("unknown ID" (of_type machine) (id (gcc x86 normal))
+         (suggestions ((localhost)))) |}]
+
+    let%expect_test "negative example: failed defaults resolution" =
+      test
+        (Act_common.Id.of_string "gcc.x86.normal")
+        ~defaults:
+          Act_common.Id.
+            [of_string "foo"; of_string "bar"; of_string "kappa"] ;
+      [%expect
+        {|
+        ("unknown ID" (of_type machine) (id (gcc x86 normal))
          (suggestions ((localhost)))) |}]
   end )
 
