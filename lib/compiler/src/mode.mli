0d86cd6c8c633bf427ada0e75f3b7cbd559b4fed
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-10 13:17:29 +0100

    Move GCC support into its own library.
    Reflecting the same split with sims and languages.

---
+++ lib/compiler/src/mode.mli
@@ -0,0 +1,15 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+(** Type of compiler run modes. *)
+type t =
+  | Assembly  (** Compiling a single file to assembly. *)
+  | Object  (** Compiling a single file to object code. *)
---
+++ lib/compiler_gcc/src/instance.ml
@@ -0,0 +1,34 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+let mode_args : Act_compiler.Mode.t -> string list = function
+  | Assembly ->
+      ["-S"]
+  | Object ->
+      ["-c"]
+
+let no_position_independence_args : string list = ["-fno-pic"]
+
+let output_args (file : string) : string list = ["-o"; file]
+
+let input_args (file : string) : string list = [file]
+
+let compile_args ~user_args ~(arch : Act_common.Id.t)
+    ~(mode : Act_compiler.Mode.t) ~infile ~outfile =
+  ignore arch ;
+  List.concat
+    [ mode_args mode
+    ; no_position_independence_args
+    ; user_args
+    ; output_args outfile
+    ; input_args infile ]
+
+let test_args = ["--version"]
