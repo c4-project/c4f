0ab353c668562c56b0212b55685ad95481fff8f1
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-14 09:36:48 +0100

    Add Jsonable and mli-ify loadable_types

--- lib/plumbing/src/loadable_types.mli
+++ lib/plumbing/src/loadable_types.mli
@@ -56,4 +56,29 @@ module type Basic_chain = sig
       of [dst]. *)
 end
 
+(** {2 JSON}
 
+    These module types are here because they're currently only used by
+    [Loadable], but may move later on. *)
+
+(** Signature of things that can be serialised to (full Yo-)json. *)
+module type To_jsonable = sig
+  type t
+
+  val to_json : t -> Yojson.t
+end
+
+(** Signature of things that can be deserialised from (full Yo-)json. *)
+module type Of_jsonable = sig
+  type t
+
+  val of_json : Yojson.t -> t
+end
+
+(** Signature of things that can be serialised to, and deserialised from,
+    (full Yo-)json. *)
+module type Jsonable = sig
+  type t
+  include To_jsonable with type t := t
+  include Of_jsonable with type t := t
+end
