3ea72e3e4442425120152ce771bb9378325844f9
Matt Windsor <m.windsor@imperial.ac.uk>
2019-06-17 12:11:27 +0100

    Aux: add one parse test, drop c_lang requirement

--- lib/litmus/test/aux.ml
+++ lib/litmus/test/aux.ml
@@ -15,14 +15,22 @@ module A = Act_litmus.Aux
 module Ac = Act_common
 
 module J = A.Json (struct
-  include Act_c_lang.Ast_basic.Constant
+  include Int
+  let to_yojson i : Yojson.Safe.t = `Int i
+  let of_yojson_exn : Yojson.Safe.t -> int = Yojson.Safe.Util.to_int
+  let of_yojson (j : Yojson.Safe.t) : (int, string) Result.t =
+    Result.try_with (fun () -> of_yojson_exn j)
+    |> Result.map_error ~f:Exn.to_string
 
-  let parse_post_string _ = Or_error.unimplemented "parse_post_string"
+  let parse_post_string (s : string) : int Act_litmus.Ast_base.Postcondition.t Or_error.t =
+    Or_error.try_with (fun () ->
+        s |> Parsexp.Single.parse_string_exn |>
+        [%of_sexp: int Act_litmus.Ast_base.Postcondition.t])
 end)
 
 let%test_module "JSON deserialisation" =
   ( module struct
-    type t = Act_c_lang.Ast_basic.Constant.t A.t
+    type t = int A.t
 
     let test (aux : t) : unit =
       let json = J.to_yojson aux in
@@ -40,19 +48,15 @@ let%test_module "JSON deserialisation" =
       let a (tid : int) = str_local tid "a" in
       let x = Ac.C_id.of_string "x" in
       let y = Ac.C_id.of_string "y" in
-      let zero = Act_c_lang.Ast_basic.Constant.Integer 0 in
-      let one = Act_c_lang.Ast_basic.Constant.Integer 1 in
-      let init : (Ac.C_id.t, Act_c_lang.Ast_basic.Constant.t) List.Assoc.t =
-        [(x, zero); (y, zero)]
+      let init : (Ac.C_id.t, int) List.Assoc.t =
+        [(x, 0); (y, 0)]
       in
-      let postcondition :
-          Act_c_lang.Ast_basic.Constant.t
-          Act_litmus.Ast_base.Postcondition.t =
+      let postcondition : int Act_litmus.Ast_base.Postcondition.t =
         Act_litmus.Ast_base.
           { quantifier= `Exists
           ; predicate=
               Pred.(
-                Elt Pred_elt.(a 0 ==? zero) && Elt Pred_elt.(a 1 ==? one))
+                Elt Pred_elt.(a 0 ==? 0) && Elt Pred_elt.(a 1 ==? 1))
           }
       in
       let locations : Ac.C_id.t list = [x; y] in
@@ -66,3 +70,23 @@ let%test_module "JSON deserialisation" =
           "postcondition": "exists (0:a == 0 /\\ 1:a == 1)"
         } |}]
   end )
+
+let%test_module "JSON serialisation" =
+  ( module struct
+    let test (json_str : string) : unit =
+      let json = Yojson.Safe.from_string json_str in
+      let aux = J.of_yojson_exn json in
+      print_s [%sexp (aux : int A.t)]
+
+    let%expect_test "SBSC example aux without a postcondition" =
+      test {|
+        {
+          "locations": [ "x", "y" ],
+          "init": { "x": 0, "y": 0 }
+        }
+        |};
+      [%expect
+        {| ((locations ((x y))) (init ((x 0) (y 0))) (postcondition ())) |}]
+  end )
+
+(* TODO(@MattWindsor91): test with postcondition *)
