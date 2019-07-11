8d55b8c85f2d50fbbc336a6ecb9231a6f7308dcb
Matt Windsor <m.windsor@imperial.ac.uk>
2019-07-11 13:39:47 +0100

    Add new 'act-fuzz' binary target
    This isn't properly separated from toplevel yet, so it's purely
    cosmetic, but eventually it'll be reasonably separate.

---
+++ bin/act_fuzz.ml
@@ -0,0 +1,16 @@
+(* The Automagic Compiler Tormentor
+
+   Copyright (c) 2018--2019 Matt Windsor and contributors
+
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
+
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
+
+(** Main entry point for the ACT fuzzer.
+
+    This will eventually replace `act c fuzz` entirely. *)
+
+let () = Core.Command.run Toplevel.C_fuzz.command
