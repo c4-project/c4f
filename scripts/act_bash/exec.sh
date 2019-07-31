f9913e4d63ffee0082f625d47e931d866174a78b
Matt Windsor <m.windsor@imperial.ac.uk>
2019-07-31 10:44:10 +0100

    Add initial fuzz checker and boilerplate
    The fuzz checker only runs both sides of the fuzz pipeline through a
    backend (eg herd) at the moment.  It should be straightforward to hook it up to
    the diff-states tool, but presently I'm blocked by the fact that A) the
    fuzzer generates Booleans, and B) herd doesn't support Booleans.  Natch.

---
+++ scripts/act_bash/exec.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+#
+# Contains a wrapper for running ACT binaries, which allows for the use
+# of 'dune exec'.
+
+# If `DUNE_EXEC` is set to "true", calls to `act::exec` will run their program
+# through `dune exec`.
+declare DUNE_EXEC
+
+# If set, overrides the choice of main `act` executable
+declare ACT
+
+
+# Runs an OCaml ACT tool.
+#
+# If `DUNE_EXEC` is set to "true", the tool will be run indirectly through
+# `dune exec`.
+#
+# Globals:
+#   - DUNE_EXEC (read)
+#
+# Arguments:
+#   1: the ACT program to execute.
+#   *: the arguments to the program.
+act::exec() {
+  local prog=$1
+  shift 1
+
+  if [[ DUNE_EXEC = "true" ]]; then
+    dune exec "${prog}" -- "$@"
+  else
+    "${prog}" "$@"
+  fi
+}
+
+# Runs the 'act-backend' tool.
+#
+# Globals:
+#   - ACT_BACKEND (read)
+#   - DUNE_EXEC (transitively read)
+#
+# Arguments:
+#   *: the arguments to the program.
+act::backend() {
+  act::exec "${ACT:-"act"}" backend "$@"
+}
--- scripts/act_bash/log.sh
+++ scripts/act_bash/log.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
 # Logging functionality.
 
+# Logging is governed by the `VERBOSE` variable, which should be set to
+# "true" to make calls to `act::log` do things.
+#
+# `VERBOSE` may be set readonly once its final value is stable.
 declare VERBOSE
+
+# Echoes the argument to stderr, prefixed by the program name.
+#
+# Arguments:
+#   1: the string to echo.
+act::error() {
+  echo "$0: $1" >&2
+}
 
 # If VERBOSE is "true", sends all subsequent arguments to printf.
 #
@@ -10,10 +22,10 @@ declare VERBOSE
 #
 # Arguments:
 #   1+: the arguments to send to printf if VERBOSE is set.
-act::log () {
+act::log() {
   if [[ "${VERBOSE}" = "true" ]]; then
     # shellcheck disable=SC2059
-    printf "${@}"
+    printf "${@}" >&2
   fi
 }
 
