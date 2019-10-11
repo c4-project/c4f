bf65cde8a08b5efdc646a1a4c7dc650fac5d329a
Matt Windsor <m.windsor@imperial.ac.uk>
2019-10-11 11:31:10 +0100

    More work on the postcondition DNFer & integration
    The DNFer seems to work fairly well, but the testing framework is a
    little faulty.  Breaking out into several issues to solve hopefully
    starting this afternoon.

--- scripts/act_bash/args.sh
+++ scripts/act_bash/args.sh
@@ -5,9 +5,9 @@
 #
 # Expects log.sh to be sourced.
 
-
+declare BACKEND
+declare COMPILER
 declare DUNE_EXEC
-
 declare VERBOSE
 
 
@@ -20,7 +20,7 @@ declare VERBOSE
 #   1: a human-readable variable name, used in errors.
 #   2: the name of the variable, used to set indirectly.
 #   3: the value to store in ${!2}.
-function act::set_nz() {
+act::set_nz() {
   local hr_name="$1"
   local name="$2"
   local value="$3"
@@ -43,7 +43,7 @@ function act::set_nz() {
 #   1: a human-readable variable name, used in errors.
 #   2: the name of the variable, used to set indirectly.
 #   3: the value to store in ${!2}.
-function act::set_nz_once() {
+act::set_nz_once() {
   local hr_name="$1"
   local name="$2"
   local value="$3"
@@ -61,7 +61,7 @@ function act::set_nz_once() {
 # Globals:
 #   DUNE_EXEC: read
 #   VERBOSE: read (transitively)
-function act::check_dune_exec() {
+act::check_dune_exec() {
    if [[ ${DUNE_EXEC} == "true" ]]; then
     act::log "%s: using 'dune exec' for ACT.\n" "$0"
   fi
@@ -74,7 +74,7 @@ function act::check_dune_exec() {
 #
 # Globals:
 #   VERBOSE: read
-function act::flags_qv() {
+act::flags_qv() {
   local flags="-q"
   if [[ ${VERBOSE} == "true" ]]; then flags="-v"; fi
   echo "${flags}"
@@ -88,8 +88,48 @@ function act::flags_qv() {
 # Globals:
 #   DUNE_EXEC: read
 #   VERBOSE: read
-function act::flags_qvx() {
-  local flags="$(act::flags_qv)"
+act::flags_qvx() {
+  local flags
+  flags="$(act::flags_qv)"
   if [[ ${DUNE_EXEC} == "true" ]]; then flags="${flags}x"; fi
   echo "${flags}"
+}
+
+
+# Runs the program in argument 1 with the quiet, verbose, and dune-exec
+# flags set, and the arguments from position 2 onwards.
+#
+# Globals:
+#   DUNE_EXEC: read (transitively)
+#   VERBOSE: read (transitivtly)
+#
+# Arguments:
+#   1. The program to run.
+#   2+. Extra arguments to supply to the program.
+act::run_with_qvx() {
+  local prog="$1"
+  shift
+
+  "${prog}" "$(act::flags_qvx)" "$@"
+}
+
+
+# Runs the program in argument 1 with the quiet, verbose, and dune-exec
+# flags set, '-b BACKEND' and '-c COMPILER', and the arguments from position 2
+# onwards.
+#
+# Globals:
+#   BACKEND: read
+#   COMPILER: read
+#   DUNE_EXEC: read (transitively)
+#   VERBOSE: read (transitivtly)
+#
+# Arguments:
+#   1. The program to run.
+#   2+. Extra arguments to supply to the program.
+function act::run_with_bcqvx() {
+  local prog="$1"
+  shift
+
+  act::run_with_qvx "${prog}" -b "${BACKEND}" -c "${COMPILER}" "$@"
 }
