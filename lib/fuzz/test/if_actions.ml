2388dd1d0daa8ecdad3b61878313d2ca47676e84
Matt Windsor <m.windsor@imperial.ac.uk>
2019-11-18 09:14:27 +0000

    Add basic if-invert AST check

--- lib/fuzz/test/if_actions.ml
+++ lib/fuzz/test/if_actions.ml
@@ -69,3 +69,16 @@ let%test_module "Surround" =
           [%expect {| (Ok (a d)) |}]
       end )
   end )
+
+let%test_module "Invert" = (module struct
+  let initial_state : Src.State.t = Lazy.force Subject.Test_data.state
+  let test : Src.Subject.Test.t = Lazy.force Subject.Test_data.test
+  let payload : Src.Path_shapes.program =
+    Src.Path_shapes.(in_func 0 @@ in_stms @@ in_stm 3 @@ this_stm)
+
+  let%expect_test "resulting AST" =
+    Action.Test_utils.run_and_dump_test
+      (Src.If_actions.Invert.run test ~payload)
+        ~initial_state;
+    [%expect]
+end)
