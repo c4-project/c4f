d636e765c16b9d02a23ebf0f71b8f9e3705281c0
Matt Windsor <m.windsor@imperial.ac.uk>
2020-01-17 10:15:57 +0000

    autofmt

--- lib/backend_litmus/src/instance.ml
+++ lib/backend_litmus/src/instance.ml
@@ -33,10 +33,7 @@ let capabilities ~(test_stdout : string list) : Bk.Capability.Summary.t =
     ~arches:(Set.of_list (module Bk.Arch) (* for now *) Bk.Arch.[c; asm_x86])
 
 let arch_map : (Act_common.Id.t, string) List.Assoc.t Lazy.t =
-  lazy
-    Act_common.Id.
-      [ (of_string "x86", "X86")
-      ; (of_string "ppc", "PPC") ]
+  lazy Act_common.Id.[(of_string "x86", "X86"); (of_string "ppc", "PPC")]
 
 let lookup_litmus_arch :
     Act_common.Id.t -> (Act_common.Id.t * string) Or_error.t =
