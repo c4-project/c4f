45a3a28d6384d5e6cb07e9d7598cf44a1839de57
Matt Windsor <m.windsor@imperial.ac.uk>
2020-05-04 14:07:24 +0100

    Stats: Traverse declarations for bool constants
    Fixes a bug whereby uses of true/false in decls wouldn't get picked up
    by the stats aggregator, causing act-litmus in the act-tester toolset to
    fail.

--- lib/c_mini/src/litmus_stats.ml
+++ lib/c_mini/src/litmus_stats.ml
@@ -26,6 +26,16 @@ let is_return (stm : Prim_statement.t) : bool =
     | Break | Continue ->
         false)
 
+let fold_fn_decl (stats : t) (decl : Initialiser.t) : t =
+  let literal_bools =
+    Option.count (Initialiser.value decl) ~f:Constant.is_bool
+  in
+  {stats with literal_bools= stats.literal_bools + literal_bools}
+
+let fold_fn_decls (decls : Initialiser.t Act_common.C_named.Alist.t)
+    (stats : t) : t =
+  List.fold decls ~init:stats ~f:(fun stats (_, d) -> fold_fn_decl stats d)
+
 let fold_top_stm (stats : t) (stm : unit Statement.t) : t =
   let returns = Stm.On_primitives.count stm ~f:is_return in
   let literal_bools = K.count stm ~f:Constant.is_bool in
@@ -33,14 +43,22 @@ let fold_top_stm (stats : t) (stm : unit Statement.t) : t =
     returns= stats.returns + returns
   ; literal_bools= stats.literal_bools + literal_bools }
 
-let fold_fn (stats : t) (fn : unit Function.t Act_common.C_named.t) : t =
-  fn |> Act_common.C_named.value |> Function.body_stms
-  |> List.fold ~init:stats ~f:fold_top_stm
+let fold_fn_stms (stms : unit Statement.t list) (stats : t) : t =
+  List.fold stms ~init:stats ~f:fold_top_stm
+
+let fold_fn (stats : t) (fn : unit Function.t) : t =
+  stats
+  |> fold_fn_decls (Function.body_decls fn)
+  |> fold_fn_stms (Function.body_stms fn)
+
+let fold_named_fn (stats : t) (fn : unit Function.t Act_common.C_named.t) : t
+    =
+  fold_fn stats (Act_common.C_named.value fn)
 
 let scrape (t : Litmus.Test.t) : t =
   let ts = Litmus.Test.threads t in
   let threads = List.length ts in
-  List.fold ts ~init:{threads; returns= 0; literal_bools= 0} ~f:fold_fn
+  List.fold ts ~init:{threads; returns= 0; literal_bools= 0} ~f:fold_named_fn
 
 let pp : t Fmt.t =
   Fmt.(
