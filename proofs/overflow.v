a4d360cefac244566327b7454c7f18234cb80b6e
Matt Windsor <m.windsor@imperial.ac.uk>
2020-11-25 11:55:36 +0000

    Add more overflow check mechanisation
    This is mainly showing completeness as well as soundness, tidying things
    up, reducing some redundant conditions I put in there the first time
    around.  This stuff, Boolean-negated, is what just went into the
    constant generator.

--- proofs/overflow.v
+++ proofs/overflow.v
@@ -127,66 +127,98 @@ Proof.
   rewrite Z.add_comm; apply add32_nonpos_no_overflow_l.
 Qed.
 
+(** We can express subtraction underflow checking in an under/overflow-free manner.
+    Note that if y = 0, we have a free choice of whether to do the check or not. *)
+Lemma sub32_under (x y : int32) :
+  { P : 0 <= `y | (proj1_sig (add_imin y P)) <= `x } \/ `y <= 0 <-> imin <= (`x - `y).
+Proof.
+  split.
+  - intros [(a & Ha) | Hyz].
+    + now rewrite <- Z.le_add_le_sub_r.
+    + destruct x, y; unfold proj1_sig, imin, imax in *; lia.
+  - intros Hoc%Z.le_add_le_sub_r.
+    destruct (ZArith_dec.Z_gt_le_dec 0 (`y)) as [Hyz|Hzy].
+    + right.
+      now apply Z.lt_le_incl, Z.gt_lt.
+    + left.
+      now exists Hzy.
+Qed.
+
+(** We can express subtraction overflow checking in an under/overflow-free manner.
+    Note that if y = 0, we have a free choice of whether to do the check or not. *)
+Lemma sub32_over (x y : int32) :
+  { P : `y <= 0 | `x <= (proj1_sig (add_imax y P)) } \/ 0 <= `y <-> (`x - `y) <= imax.
+Proof.
+  split.
+  - intros [(a & Ha) | Hzy].
+    + now rewrite Z.le_sub_le_add_r.
+    + destruct x, y; unfold proj1_sig, imin, imax in *; lia.
+  - intros Hoc%Z.le_sub_le_add_r.
+    destruct (ZArith_dec.Z_gt_le_dec (`y) 0) as [Hzy|Hyz].
+    + right.
+      now apply Z.lt_le_incl, Z.gt_lt.
+    + left.
+      now exists Hyz.
+Qed.
+
 (** Safe arbitrary subtraction, given computed overflow and underflow checks. *)
 Definition sub32
            (x y : int32)
-           (Hunder : { P : 0 <= `y | (proj1_sig (add_imin y P)) <= `x } + (0 <= `x \/ `y <= 0))
-           (Hover  : { P : `y <= 0 | `x <= (proj1_sig (add_imax y P)) } + (0 <= `y \/ `x < 0))
+           (Hunder : { P : 0 <= `y | (proj1_sig (add_imin y P)) <= `x } \/ (`y <= 0))
+           (Hover  : { P : `y <= 0 | `x <= (proj1_sig (add_imax y P)) } \/ (0 <= `y))
  : int32.
 Proof.
   refine (mki32 (`x - `y) _).
   split.
-  - destruct Hunder as [(Hzly & Hylx)|[Hxp|Hyn]].
-    + rewrite <- Z.le_add_le_sub_r.
-      destruct (add_imin y Hzly) as (a & Ha) eqn:Hae.
-      destruct x as (x & Hx), y as (y & Hy).
-      apply proj1_sig_eq in Hae.
-      rewrite add_imin_unfold in Hae.
-      unfold proj1_sig in *.
-      now subst.
-    + now apply sub32_nonneg_no_underflow_l.
-    + now apply sub32_nonpos_no_underflow_r.
-  - destruct Hover as [(Hylz & Hxly)|[Hyp|Hxn]].
-    + rewrite Z.le_sub_le_add_r.
-      destruct (add_imax y Hylz) as (a & Ha) eqn:Hae.
-      destruct x as (x & Hx), y as (y & Hy).
-      apply proj1_sig_eq in Hae.
-      rewrite add_imax_unfold in Hae.
-      unfold proj1_sig in *.
-      now subst.
-    + now apply sub32_nonneg_no_overflow_r.
-    + now apply sub32_neg_no_overflow_l.
+  - now apply sub32_under.
+  - now apply sub32_over.
 Defined.
+
+(** We can express addition underflow checking in an under/overflow-free manner.
+    Note that if y = 0, we have a free choice of whether to do the check or not;
+    also note that commutativity means we can flip to overflow checking on [x]. *)
+Lemma add32_under (x y : int32) :
+ { P : `y <= 0 | (proj1_sig (sub_imin y P)) <= `x } \/ 0 <= `y <-> imin <= (`x + `y).
+Proof.
+  split.
+  - intros [(a & Ha) | Hyz].
+    + now rewrite <- Z.le_sub_le_add_r.
+    + destruct x, y; unfold proj1_sig, imin, imax in *; lia.
+  - intros Hoc%Z.le_sub_le_add_r.
+    destruct (ZArith_dec.Z_gt_le_dec 0 (`y)) as [Hyz|Hzy]; intuition.
+    left.
+    apply Z.gt_lt, Z.lt_le_incl in Hyz.
+    now exists Hyz.
+Qed.
+
+(** We can express addition overflow checking in an under/overflow-free manner.
+    Note that if y = 0, we have a free choice of whether to do the check or not;
+    also note that commutativity means we can flip to overflow checking on [x]. *)
+Lemma add32_over (x y : int32) :
+  { P : 0 <= `y | `x <= (proj1_sig (sub_imax y P)) } \/ `y <= 0 <-> (`x + `y) <= imax.
+Proof.
+  split.
+  - intros [(a & Ha) | Hzy].
+    + now rewrite Z.le_add_le_sub_r.
+    + destruct x, y; unfold proj1_sig, imin, imax in *; lia.
+  - intros Hoc%Z.le_add_le_sub_r.
+    destruct (ZArith_dec.Z_gt_le_dec (`y) 0) as [Hzy|Hyz]; intuition.
+    left.
+    apply Z.gt_lt, Z.lt_le_incl in Hzy.
+    now exists Hzy.
+Qed.
 
 (** Safe arbitrary addition, given computed overflow and underflow checks. *)
 Definition add32
            (x y : int32)
-           (Hunder : { P : `y <= 0 | (proj1_sig (sub_imin y P)) <= `x } + (0 <= `x \/ 0 <= `y))
-           (Hover  : { P : 0 <= `y | `x <= (proj1_sig (sub_imax y P)) } + (`x <= 0 \/ `y <= 0))
+           (Hunder : { P : `y <= 0 | (proj1_sig (sub_imin y P)) <= `x } \/ 0 <= `y)
+           (Hover  : { P : 0 <= `y | `x <= (proj1_sig (sub_imax y P)) } \/ `y <= 0)
  : int32.
 Proof.
   refine (mki32 (`x + `y) _).
   split.
-  - destruct Hunder as [(Hylz & Hylx)|[Hxp|Hyn]].
-    + rewrite <- Z.le_sub_le_add_r.
-      destruct (sub_imin y Hylz) as (a & Ha) eqn:Hae.
-      destruct x as (x & Hx), y as (y & Hy).
-      apply proj1_sig_eq in Hae.
-      rewrite sub_imin_unfold in Hae.
-      unfold proj1_sig in *.
-      now subst.
-    + now apply add32_nonneg_no_underflow_l.
-    + now apply add32_nonneg_no_underflow_r.
-  - destruct Hover as [(Hzly & Hxly)|[Hyp|Hxn]].
-    + rewrite Z.le_add_le_sub_r.
-      destruct (sub_imax y Hzly) as (a & Ha) eqn:Hae.
-      destruct x as (x & Hx), y as (y & Hy).
-      apply proj1_sig_eq in Hae.
-      rewrite sub_imax_unfold in Hae.
-      unfold proj1_sig in *.
-      now subst.
-    + now apply add32_nonpos_no_overflow_l.
-    + now apply add32_nonpos_no_overflow_r.
+  - now apply add32_under.
+  - now apply add32_over.
 Defined.
 
 (** We can forget that a subtraction doesn't underflow or overflow. *)
@@ -201,3 +233,46 @@ Proof.
   trivial.
 Qed.
 
+(* WORK IN PROGRESS
+
+Program Definition zero32 : int32 := mki32 0 _.
+Next Obligation.
+  unfold imin, imax; lia.
+Defined.
+
+Program Definition imin32 : int32 := mki32 imin _.
+Next Obligation.
+  unfold imin, imax; lia.
+Defined.
+
+Program Definition imax32 : int32 := mki32 imax _.
+Next Obligation.
+  unfold imin, imax; lia.
+Defined.
+
+Definition sub32_compute (x y : int32) : (int32 * int32).
+Proof.
+  refine (
+      match Z.compare (`y) 0 as yz, Z.compare (`x) 0 as xz return Z.compare (`y) 0 = yz -> Z.compare (`x) 0 = xz -> _ with
+      | Eq, _ => fun _ _ => (zero32, x)
+      | Lt, Lt | Gt, Eq | Gt, Gt => fun Hyzl Hxzl => (zero32, sub32 x y _ _)
+      | Lt, Eq | Lt, Gt => fun Hyzl Hxzl => (* Overflow *) (imax32, sub32 (add32 x imax32 _ _) y _ _)
+      | Gt, Lt => fun Hyzl Hxzl => (* Underflow *) (imin32, sub32 (add32 x imin32 _ _) y _ _)
+      end _ _
+    ).
+  - destruct x as (x & Hx), y as (y & Hy); unfold proj1_sig in *.
+    apply Z.compare_eq in Hxzl.
+    subst.
+    right.
+    now left.
+  - apply Z.compare_eq in Hxzl.
+    apply -> Z.compare_lt_iff in Hyzl.
+    left.
+    apply Z.lt_le_incl in Hyzl.
+    apply exist with (x := Hyzl).
+    rewrite add_imax_unfold.
+    destruct x, y; unfold proj1_sig, imax, imin in *; subst.
+                            lia.
+
+    
+*)
