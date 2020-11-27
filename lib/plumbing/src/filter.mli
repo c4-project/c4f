268b474c8620695f09bb0cb7f7fdc9b8bd4b115c
Matt Windsor <m.windsor@imperial.ac.uk>
2020-11-27 16:32:52 +0000

    Delete more unused Plumbing cruft
    I'm getting close to deleting Filter, but it'll need some gradual
    inlining.

--- lib/plumbing/src/filter.mli
+++ lib/plumbing/src/filter.mli
@@ -1,24 +1,13 @@
-(* This file is part of 'act'.
+(* The Automagic Compiler Tormentor
 
-   Copyright (c) 2018, 2019 by Matt Windsor
+   Copyright (c) 2018, 2019, 2020 Matt Windsor and contributors
 
-   Permission is hereby granted, free of charge, to any person obtaining a
-   copy of this software and associated documentation files (the "Software"),
-   to deal in the Software without restriction, including without limitation
-   the rights to use, copy, modify, merge, publish, distribute, sublicense,
-   and/or sell copies of the Software, and to permit persons to whom the
-   Software is furnished to do so, subject to the following conditions:
+   ACT itself is licensed under the MIT License. See the LICENSE file in the
+   project root for more information.
 
-   The above copyright notice and this permission notice shall be included in
-   all copies or substantial portions of the Software.
-
-   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-   DEALINGS IN THE SOFTWARE. *)
+   ACT is based in part on code from the Herdtools7 project
+   (https://github.com/herd/herdtools7) : see the LICENSE.herd file in the
+   project root for more information. *)
 
 (** Modules for building 'filters', in the UNIX sense.
 
@@ -26,33 +15,13 @@
     in another. This module contains signatures and functors for building and
     composing filters over the [Io] abstractions. *)
 
-open Base
+(* TODO(@MattWindsor91): this module is deprecated and should be removed
+   eventually. *)
+
 open Filter_types
 
 (** {2 Making filters} *)
 
 (** Makes a filter from a {{!Basic} Basic}. *)
 module Make (B : Basic) :
-  S with type aux_i = B.aux_i and type aux_o = B.aux_o
-
-(** Makes a filter from a {{!Basic_in_file_only} Basic_in_file_only}. *)
-module Make_in_file_only (B : Basic_in_file_only) :
-  S with type aux_i = B.aux_i and type aux_o = B.aux_o
-
-(** Makes a filter from a {{!Basic_in_file_only} Basic_files_only}. *)
-module Make_files_only (B : Basic_files_only) :
-  S with type aux_i = B.aux_i and type aux_o = B.aux_o
-
-(** Makes an in-file-only filter by combining a {{!Runner.S} runner} and some
-    program accessors. *)
-module Make_on_runner (R : Basic_on_runner) :
-  S with type aux_i = R.aux_i and type aux_o = unit
-
-(** {2 Adapting filters}
-
-    Filter chaining is in {{!Filter_chain} another module}. *)
-
-(** Adapts the auxiliary types of an existing filter, performing partial
-    mappings to and from a new pair of auxiliary types. *)
-module Adapt (B : Basic_adapt) :
   S with type aux_i = B.aux_i and type aux_o = B.aux_o
--- lib/plumbing/src/filter_types.ml
+++ lib/plumbing/src/filter_types.ml
@@ -1,6 +1,6 @@
 (* The Automagic Compiler Tormentor
 
-   Copyright (c) 2018--2019 Matt Windsor and contributors
+   Copyright (c) 2018, 2019, 2020 Matt Windsor and contributors
 
    ACT itself is licensed under the MIT License. See the LICENSE file in the
    project root for more information.
@@ -36,67 +36,10 @@ module type Basic = sig
     -> aux_o Or_error.t
 end
 
-(** Input signature for filters that require physical files in input
-    position. *)
-module type Basic_in_file_only = sig
-  include Common
-
-  val run :
-    aux_i Filter_context.t -> Fpath.t -> Out_channel.t -> aux_o Or_error.t
-end
-
-(** Input signature for filters that require physical files in both input and
-    output position. *)
-module type Basic_files_only = sig
-  include Common
-
-  val run :
-       aux_i Filter_context.t
-    -> infile:Fpath.t
-    -> outfile:Fpath.t
-    -> aux_o Or_error.t
-end
-
 module type S = sig
   include Common
 
   val run : aux_i -> Input.t -> Output.t -> aux_o Or_error.t
   (** [run aux source sink] runs this filter on [source], outputs to [sink],
       reads and returns any auxiliary state on success. *)
-end
-
-(** Signature of inputs needed to adapt a filter. *)
-module type Basic_adapt = sig
-  (** The original filter. *)
-  module Original : S
-
-  (** The new input type. *)
-  type aux_i
-
-  (** The new output type. *)
-  type aux_o
-
-  val adapt_i : aux_i -> Original.aux_i Or_error.t
-  (** [adapt_i aux] tries to adapt the new input type to the old one. *)
-
-  val adapt_o : Original.aux_o -> aux_o Or_error.t
-  (** [adapt_i aux] tries to adapt the old output type to the new one. *)
-end
-
-(** Basic signature for building filters from external programs on top of a
-    [Runner]. *)
-module type Basic_on_runner = sig
-  include Common with type aux_o := unit
-
-  (** The runner to use to run the program. *)
-  module Runner : Runner_types.S
-
-  val prog : aux_i -> string
-  (** [prog aux] gets the name of the program to run, given the auxiliary
-      input [aux]. *)
-
-  val argv : aux_i -> string -> string list Or_error.t
-  (** [argv aux file] gets the argument vector to supply, given the auxiliary
-      input [aux] and input file [file]. It can fail if the inputs given do
-      not correspond to a valid invocation of the program. *)
 end
